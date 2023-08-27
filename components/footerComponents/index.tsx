7c423e9fbbccf506c1cf5854f816dca002ac77f0
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2023-08-28 04:18:40 +0700

    Remove excess files (4)

--- components/footerComponents/index.tsx
+++ components/footerComponents/index.tsx
@@ -1,17 +1,10 @@
-import { SocialMediaLinks } from '../design-system/SocialMediaLinks'
-
 const Footer = () => {
   const currentYear = new Date().getFullYear()
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 py-4 md:px-8 text-white bg-[#013640] justify-center items-center">
-      <div className="px-6 md:px-8 pb-4 text-biography-sm">
-        Building 59H, Preah Ang Eng St (13), Phsar Kandal 1, Daun Penh, Phnom
-        Penh City, 120205
-      </div>
+      <div className="px-6 md:px-8 pb-4 text-biography-sm">asdf</div>
       <div className="px-6 md:px-8">
-        <div className="flex flex-row justify-end items-end mb-4">
-          <SocialMediaLinks />
-        </div>
+        <div className="flex flex-row justify-end items-end mb-4">asdf</div>
         <div className="text-biography-sm flex flex-row justify-end items-end">
           &copy; {currentYear}
         </div>
