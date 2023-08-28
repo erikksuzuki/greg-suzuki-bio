7dd733e35663e09f10a5ed7ab34f87077839ac5e
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2023-08-28 11:14:52 +0700

    fix lightbox

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 'use client'
-
+import { useRouter } from 'next/router'
 import dynamic from 'next/dynamic'
 import { NextSeo } from 'next-seo'
 import { useQueryParam, StringParam, withDefault } from 'use-query-params'
 
 import 'tiny-slider/dist/tiny-slider.css'
 
-import Lightbox, { ImagesListType } from 'react-spring-lightbox'
+// import Lightbox from 'react-spring-lightbox'
 import ImageCrossFade from '../components/ImageCrossFade'
 import WritingPageContent from '../components/pageComponents/WritingPageContent'
 import MusicPageContent from '../components/pageComponents/MusicPageContent'
@@ -15,67 +15,78 @@ import BiographyPageContent from '../components/pageComponents/BiogrpahyPageCont
 import NavigationDesktop from '../components/navigationComponents/NavigationDesktop'
 import { useState } from 'react'
 
+import 'photoswipe/dist/photoswipe.css'
+
+import { Gallery, Item } from 'react-photoswipe-gallery'
+
 const TinySlider: any = dynamic(() => import('tiny-slider-react'), {
   ssr: false,
 })
 
-const sideImages: ImagesListType = [
+const sideImages: any = [
   {
     src: '/sidepics/budokan.jpg',
     loading: 'lazy',
     alt: 'Live in Budokan at Technics, Japan Jazz Festival (1982)',
+    index: 0,
   },
   {
     src: '/sidepics/business.jpg',
     loading: 'lazy',
     alt: 'Feld Entertainment directors meeting in France (2007)',
+    index: 1,
   },
   {
     src: '/sidepics/77078_4971773214605_1009990067_n.jpg',
     loading: 'lazy',
     alt: 'Star Trek The Exhibition',
+    index: 2,
   },
   {
     src: '/sidepics/dadinegypt.jpg',
     loading: 'lazy',
     alt: 'Greg Suzuki in Egypt',
+    index: 3,
   },
   {
     src: '/sidepics/fatherandson.jpg',
     loading: 'lazy',
     alt: 'Greg and Fumio Suzuki (2009)',
+    index: 4,
   },
   {
     src: '/sidepics/jamsession.jpg',
     loading: 'lazy',
     alt: 'Greg with friends Kato Hisanori and Bobby Masuyama',
+    index: 5,
   },
   {
     src: '/sidepics/liveinlosangeles.jpg',
     loading: 'lazy',
     alt: 'Live in Los Angeles',
+    index: 6,
   },
   {
     src: '/sidepics/liveinsingapore.jpg',
     loading: 'lazy',
     alt: 'Live in Singapore',
+    index: 7,
   },
   {
     src: '/sidepics/younggreg1.jpg',
     loading: 'lazy',
     alt: 'Junior year birthday party with Fumitaka John Torio, Koichi Nagai and Yoshio Nagai',
+    index: 8,
   },
   {
     src: '/sidepics/younggreg2.jpg',
     loading: 'lazy',
     alt: 'After concert with Takehisa Hirayama, Youn Ho Park and Papillong Lee',
+    index: 9,
   },
 ]
 
-export const PhotoSliderVerticle = ({
-  setLightboxOpen,
-  setCurrentIndex,
-}: any) => {
+export const PhotoSliderVerticle = () => {
   const settings = {
     nav: false,
     controls: false,
@@ -98,19 +109,19 @@ export const PhotoSliderVerticle = ({
         {sideImages.map((image: any, index: number) => {
           return (
             <div className="relative w-[140px]" key={`sideimage-${index}`}>
-              <img
-                src={image.src}
-                className="cursor-pointer w-full opacity-50 hover:opacity-100 transition-all"
-                onClick={() => {
-                  setCurrentIndex(index)
-                  setLightboxOpen(true)
-                }}
-                style={{
-                  transitionProperty: 'all',
-                  transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
-                  transitionDuration: '400ms',
-                }}
-              />
+              <a href={`#&gid=sideImages&pid=image-${image.index}`}>
+                <img
+                  rel="lightbox[roadtrip]"
+                  id={`slideid-${image.index}`}
+                  src={image.src}
+                  className="cursor-pointer w-full opacity-50 hover:opacity-100 transition-all"
+                  style={{
+                    transitionProperty: 'all',
+                    transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+                    transitionDuration: '400ms',
+                  }}
+                />
+              </a>
             </div>
           )
         })}
@@ -119,61 +130,37 @@ export const PhotoSliderVerticle = ({
   )
 }
 
-export const PhotoModal = ({
-  lightboxOpen,
-  setLightboxOpen,
-  currentImageIndex,
-  setCurrentIndex,
-}: any) => {
-  const handleClose = () => setLightboxOpen(false)
-  const gotoPrevious = () =>
-    currentImageIndex > 0 && setCurrentIndex(currentImageIndex - 1)
-  const gotoNext = () =>
-    currentImageIndex + 1 < sideImages.length &&
-    setCurrentIndex(currentImageIndex + 1)
-
-  const CaptionComponent = () => {
-    return (
-      <div className="bg-black text-center text-white py-6">
-        {sideImages[currentImageIndex].alt}
-      </div>
-    )
+export const PhotoModal = ({ page }: any) => {
+  const router = useRouter()
+  const onBeforeOpen = (pswpInstance: any) => {
+    router.replace(`/?page=${page}`)
+    pswpInstance.on('change', () => {
+      router.replace(`/?page=${page}`)
+    })
   }
   return (
-    <Lightbox
-      isOpen={lightboxOpen}
-      onClose={handleClose}
-      onPrev={gotoPrevious}
-      onNext={gotoNext}
-      images={sideImages}
-      currentIndex={currentImageIndex}
-      singleClickToZoom
-      style={{ background: 'rgba(0,0,0,0.5)' }}
-      className=""
-      renderFooter={() => <CaptionComponent />}
-      pageTransitionConfig={{
-        from: { opacity: 0 },
-        enter: { opacity: 1 },
-        leave: { opacity: 0 },
-        config: { mass: 1, tension: 320, friction: 32 },
-      }}
-
-      /* Add your own UI */
-      // renderHeader={() => (<CustomHeader />)}
-      // renderPrevButton={() => (<CustomLeftArrowButton />)}
-      // renderNextButton={() => (<CustomRightArrowButton />)}
-      // renderImageOverlay={() => (<ImageOverlayComponent >)}
-
-      /* react-spring config for open/close animation */
-    />
+    <Gallery id="sideImages" onBeforeOpen={onBeforeOpen}>
+      {sideImages.map((image: any) => {
+        return (
+          <Item
+            key={`sideImages-image-${image.index}`}
+            id={`image-${image.index}`}
+            original={image.src}
+            thumbnail={image.src}
+            width="1280"
+            height="960"
+          >
+            {({ ref, open }: any) => (
+              <img ref={ref} onClick={open} src={image.src} />
+            )}
+          </Item>
+        )
+      })}
+    </Gallery>
   )
 }
 
-export function BackgroundImage({
-  backgroundImage,
-}: {
-  backgroundImage: string
-}) {
+export function BackgroundImage({ backgroundImage }: any) {
   return <ImageCrossFade imgUrl={backgroundImage} width={1728} height={864} />
 }
 
@@ -202,14 +189,14 @@ export default function Home() {
           </header>
           <div className="max-w-screen-xl mx-auto">
             <div className="flex flex-col md:flex-row w-full ">
-              <nav className="flex-grow max-h-[495px]">
+              <nav className="flex-grow max-h-[490px]">
                 <NavigationDesktop
                   setPage={setPage}
                   setBackgroundImage={setBackgroundImage}
                 />
               </nav>
               <main
-                className="flex-grow md:max-h-[495px] md:overflow-scroll"
+                className="flex-grow md:max-h-[490px] md:overflow-scroll"
                 style={{
                   boxShadow: '2px -1px 19px 0px rgba(0,0,0,0.75) inset',
                 }}
@@ -219,21 +206,15 @@ export default function Home() {
                 {page === 'music' && <MusicPageContent />}
                 {page === 'writing' && <WritingPageContent />}
               </main>
-              <aside className="p-4 min-w-[172px] max-h-[495px] overflow-y-hidden">
-                <PhotoSliderVerticle
-                  setLightboxOpen={setLightboxOpen}
-                  setCurrentIndex={setCurrentIndex}
-                />
+              <aside className="p-4 min-w-[172px] max-h-[490px] overflow-y-hidden">
+                <PhotoSliderVerticle />
               </aside>
             </div>
           </div>
         </section>
-        <PhotoModal
-          lightboxOpen={lightboxOpen}
-          setLightboxOpen={setLightboxOpen}
-          currentImageIndex={currentImageIndex}
-          setCurrentIndex={setCurrentIndex}
-        />
+        <div className="overflow-hidden w-[1px] h-[1px]">
+          <PhotoModal page={page} />
+        </div>
       </div>
     </>
   )
